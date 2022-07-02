805ae588279993c5577038236913aaa5ca1b3777
darshanpatel2 <107154778+darshanpatel2@users.noreply.github.com>
2022-07-02 13:34:37 -0400

    Darshan version 1.1

---
+++ Darshangittest/src/main/java/com/mycompany/darshangittest/Darshangittest.java
@@ -0,0 +1,8 @@
+package com.mycompany.darshangittest;
+
+public class Darshangittest {
+
+    public static void main(String[] args) {
+        System.out.println("This is Darshan in Main Directory");
+    }
+}
