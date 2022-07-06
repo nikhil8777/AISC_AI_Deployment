8badedd937fe7c883691315f16a87a2a8bd8cabb
nikhil8777 <nikhil@loyalistcollege.com>
2022-07-06 00:01:27 -0400

    Version_1(Nikhil-500188246)

---
+++ AISC_AI_project/src/main/java/com/mycompany/aisc_ai_project/AISC_AI_project.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.aisc_ai_project;
+
+/**
+ *
+ * @author nikhil
+ */
+public class AISC_AI_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Python!");
+    }
+}
